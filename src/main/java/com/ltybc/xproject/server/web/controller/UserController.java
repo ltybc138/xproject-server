c742d600cac6965e631f56e13156ea927a560b67
denis <semenov200108@gmail.com>
2018-08-16 17:26:36 +0500

    Added CrossOrigin annotation to every Controller class for further using it in third party apps(or frontend)

--- src/main/java/com/ltybc/xproject/server/web/controller/UserController.java
+++ src/main/java/com/ltybc/xproject/server/web/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @Transactional
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserService userService;
